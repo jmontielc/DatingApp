204b69f9c0324e822eb4012b13b94cf4f537043b
jmontielc <lia.julian@gmail.com>
2019-10-06 15:02:38 -0700

    End of section 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -48,7 +48,7 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-        {
+        {        
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
             
             if (userFromRepo == null)
@@ -62,7 +62,7 @@ namespace DatingApp.API.Controllers
 
             var key = new SymmetricSecurityKey(Encoding.UTF8
                 .GetBytes(_config.GetSection("AppSettings:Token").Value));
-           
+        
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -78,7 +78,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new{
                 token = tokenHandler.WriteToken(token)
-            });            
+            });                      
         }
     }
 }
